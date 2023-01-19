c71af618b0a612030d14a92dfcc5a93e8b72c28b
Håvard Hetland Vestbø <haavarhv@stud.ntnu.no>
2023-01-19 10:58:27 +0100

    Initialize Springboot

---
+++ Backend/ProFlex/src/main/java/no/ntnu/ProFlex/ProFlexApplication.java
@@ -0,0 +1,13 @@
+package no.ntnu.ProFlex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProFlexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProFlexApplication.class, args);
+	}
+
+}
---
+++ Backend/ProFlex/src/test/java/no/ntnu/ProFlex/ProFlexApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.ProFlex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProFlexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
