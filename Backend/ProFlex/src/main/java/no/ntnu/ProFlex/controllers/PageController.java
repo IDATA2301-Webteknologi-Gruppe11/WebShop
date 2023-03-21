18e0657ff041e6c86def9abda52d4149bcec6b06
Håvard Hetland Vestbø <haavarhv@stud.ntnu.no>
2023-03-21 10:58:29 +0100

    Update javadoc PageController

--- Backend/ProFlex/src/main/java/no/ntnu/ProFlex/controllers/PageController.java
+++ Backend/ProFlex/src/main/java/no/ntnu/ProFlex/controllers/PageController.java
@@ -7,32 +7,54 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import no.ntnu.ProFlex.services.ProductService;
 
-/*
- * Serves the HTML documents with help of Thymeleaf templates. 
- */
+/**
+ * Serves the HTML documents with help of Thymeleaf templates.
+ * 
+ * @author Håvard Hetland Vesbø
+ * @version 1.0
+ **/
 @Controller
 public class PageController {
-
     @Autowired
     ProductService productService;
     @Autowired
     ProductRepository productRepository;
-    
+
+    /**
+     * Serve the "Home" page
+     *
+     * @return Name of the ThymeLeaf template which will be used to render the HTML
+     */
     @GetMapping("/")
     public String getIndex(Model model) {
         return "Index";
     }
 
+    /**
+     * Serve the "Profile" page
+     *
+     * @return Name of the ThymeLeaf template which will be used to render the HTML
+     */
     @GetMapping("/Profile")
     public String getProfile(Model model) {
         return "Profile";
     }
 
+    /**
+     * Serve the "Products" page
+     *
+     * @return Name of the ThymeLeaf template which will be used to render the HTML
+     */
     @GetMapping("/Products")
     public String getProducts(Model model) {
         return "Products";
     }
 
+    /**
+     * Serve the "ShoppingCart" page
+     *
+     * @return Name of the ThymeLeaf template which will be used to render the HTML
+     */
     @GetMapping("/ShoppingCart")
     public String getShoppingCart(Model model) {
         return "ShoppingCart";
