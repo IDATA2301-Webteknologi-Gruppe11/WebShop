3393c1068bb796a20ff8b449a84ac609f9f7d65e
Håvard Hetland Vestbø <haavarhv@stud.ntnu.no>
2023-05-19 16:48:17 +0200

    Minor fix Header.js

--- src/main/resources/static/js/Header.js
+++ src/main/resources/static/js/Header.js
@@ -3,8 +3,8 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
 
-  if (prevScrollpos > currentScrollPos) {
-    // Scrolling up, show the header
+  if (prevScrollpos >= currentScrollPos || currentScrollPos === 0) {
+    // Scrolling up or at the top, show the header
     document.querySelector(".header").style.display = "block";
   } else {
     // Scrolling down, hide the header
